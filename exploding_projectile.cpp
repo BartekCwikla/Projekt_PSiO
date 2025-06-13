a5451c67690f258ea30233d6e02bfe48597013c6
Bartosz Ćwikła <bartosz.cwikla@icloud.com>
2025-06-13 20:27:34 +0200

    Fixed lagging double and quad gun

--- exploding_projectile.cpp
+++ exploding_projectile.cpp
@@ -3,6 +3,8 @@
 
 ExplodingProjectile::ExplodingProjectile(sf::Vector2f dir, sf::Vector2f initial_position, float speed, float max_distance, float damage, float explosion_radius) : Projectile(dir, initial_position, speed, max_distance, damage), explosionRadius(explosion_radius) {
     setIsExploding(true);
+    sound.loadSoundEffect("gun", "./assets/Sounds/Sounds/gun.wav", 35.f, false);
+    sound.playSoundEffect("gun");
 }
 
 
